683c9e00dccbb8bfc8191c8e0b2880cd4206f6a1
Kengwang <github@kengwang.com.cn>
2024-03-02 21:47:01 +0800

    [feat] Config logger to console

--- LyricDb.Web/Endpoints/LoggerEndpoint.cs
+++ LyricDb.Web/Endpoints/LoggerEndpoint.cs
@@ -8,9 +8,8 @@ public class LoggerEndpoint : IEndpointBase
     public static void ConfigureBuilder(WebApplicationBuilder builder)
     {
         Log.Logger = new LoggerConfiguration()
-#if DEBUG
             .WriteTo.Console()
-#else
+#if !DEBUG
             .WriteTo.File("log/log.log", rollingInterval: RollingInterval.Day)
 #endif
             .CreateLogger();
