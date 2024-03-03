bd2b57403fdefbe75d57d200d3eb08060bd9b713
Kengwang <github@kengwang.com.cn>
2024-03-03 16:01:28 +0800

    [chore] 添加 Swagger 挂在服务器

--- LyricDb.Web/Endpoints/SwaggerEndpoint.cs
+++ LyricDb.Web/Endpoints/SwaggerEndpoint.cs
@@ -1,4 +1,5 @@
 ﻿using LyricDb.Web.Interfaces;
+using Microsoft.OpenApi.Models;
 
 namespace LyricDb.Web.Endpoints;
 
@@ -7,7 +8,19 @@ public class SwaggerEndpoint : IEndpointBase
     public static void ConfigureBuilder(WebApplicationBuilder builder)
     {
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.AddServer(new OpenApiServer
+            {
+                Description = "Production",
+                Url = "https://lyricdb.kengwang.com.cn/api/"
+            });
+            options.AddServer(new OpenApiServer
+            {
+                Description = "Development",
+                Url = "http://localhost:5140/"
+            });
+        });
     }
 
     public static void ConfigureApp(WebApplication app)
